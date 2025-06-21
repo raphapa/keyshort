3b441a77fa05811e597945be056483fdf61a4a53
raphel <131486330+raphapa@users.noreply.github.com>
2025-06-21 17:43:59 +0200

    ajout du premier racourci test

--- main.py
+++ main.py
@@ -1,7 +1,18 @@
+import keyboard
+
 version = "v0.1"
-def main():
+def action():
+    print("hello world")
+func = [action]
+def main(*args):
     global version
-    print("demarge du program version " + version)
+    global func
+    print("demarrage du programme version " + version)
+    keyboard.add_hotkey('ctrl+shift+a', func[0])
+    while True:
+        pass
+
 print(__name__)
-if __name__ == "main"
-    main():
+
+if __name__ == "__main__":
+    main()
