2304e34109a18cc77638dad70257be10f43047b3
raphel <131486330+raphapa@users.noreply.github.com>
2025-06-22 08:18:48 +0200

    we can now on off the keyboardshortcut extension

--- main.py
+++ main.py
@@ -1,18 +1,29 @@
 import keyboard
 
-version = "v0.1"
+version = "v0.2"
+keyboardextensionstate = False
+def set_keyboard_extension_state():
+    global keyboardextensionstate
+    if keyboardextensionstate:
+        keyboardextensionstate = False
+    else:
+        keyboardextensionstate = True
 def action():
-    print("hello world")
-func = [action]
+    global keyboardextensionstate
+    if keyboardextensionstate:
+        print("Action triggered!")
+    else:
+        pass
+    # Here you can add the code that you want to execute when the hotkey is pressed
+func = [set_keyboard_extension_state, action]
 def main(*args):
     global version
     global func
+    global keyboardextensionstate
     print("demarrage du programme version " + version)
-    keyboard.add_hotkey('ctrl+shift+a', func[0])
+    keyboard.add_hotkey('ctrl+shift+a', func[1])
+    keyboard.add_hotkey('ctrl+k+x', func[0])
     while True:
         pass
-
-print(__name__)
-
 if __name__ == "__main__":
     main()
