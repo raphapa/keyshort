5b4079e256aafbf09fbf29048aa1a9adeca8b589
raphel <131486330+raphapa@users.noreply.github.com>
2025-06-22 09:06:38 +0200

    we can now creat basique project with crtl+alt+b+p

--- main.py
+++ main.py
@@ -1,29 +1,51 @@
+import os
 import keyboard
+import time
 
-version = "v0.3"
+version = "v0.4"
 keyboardextensionstate = False
+
 def set_keyboard_extension_state():
     global keyboardextensionstate
-    if keyboardextensionstate:
-        keyboardextensionstate = False
-    else:
-        keyboardextensionstate = True
+    keyboardextensionstate = not keyboardextensionstate
+
 def action():
     global keyboardextensionstate
     if keyboardextensionstate:
         print("Action triggered!")
-    else:
-        pass
-    # Here you can add the code that you want to execute when the hotkey is pressed
+
+def create_basic_project():
+    base_path = os.path.expanduser("~/Desktop")
+    project_path = os.path.join(base_path, "BasicProject")
+
+    try:
+        os.makedirs(project_path, exist_ok=True)
+
+        # Créer les sous-dossiers v1 à v10
+        for i in range(1, 11):
+            os.makedirs(os.path.join(project_path, f"v{i}"), exist_ok=True)
+
+        # Créer les sous-dossiers vdef-beta et vdef-fin
+        os.makedirs(os.path.join(project_path, "vdef-beta"), exist_ok=True)
+        os.makedirs(os.path.join(project_path, "vdef-fin"), exist_ok=True)
+
+        # Message d'information sans emoji
+        print("Projet basique créé dans :", project_path)
+
+    except Exception as e:
+        print("Erreur lors de la création du projet :", e)
+
 func = [set_keyboard_extension_state, action]
+
 def main(*args):
-    global version
-    global func
-    global keyboardextensionstate
-    print("demarrage du programme version " + version)
+    print("Démarrage du programme version", version)
     keyboard.add_hotkey('ctrl+shift+a', func[1])
     keyboard.add_hotkey('ctrl+k+x', func[0])
+    keyboard.add_hotkey('ctrl+alt+b+p', create_basic_project)
+    keyboard.add_hotkey('esc', lambda: exit())
+
     while True:
-        pass
+        time.sleep(0.1)
+
 if __name__ == "__main__":
     main()
